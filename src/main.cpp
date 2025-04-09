71450fc67b5878866e9dafc38f9a75509ab4cfe0
strangeweirdo19 <kirtivas.s2k1@gmail.com>
2025-04-09 22:09:04 +0530

    added png

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 // this sketch is will not run on smaller memory processors (e.g.
 // ESP8266, STM32F103 etc.)
 
+// It uses DMA to send image data to the TFT while the decoding takes
+// place. The processor and display combination must support DMA to
+// run this sketch! The decode time dominates so DMA is mainly an advantage
+// for slow display interface speeds.
+
 // The test image is in the sketch "data" folder (press Ctrl+K to see it).
 // You must upload the image to LittleFS using the Arduino IDE Tools Data
 // Upload menu option (you may need to install extra tools for that).
@@ -31,6 +36,31 @@ int16_t ypos = 0;
 #include <TFT_eSPI.h>              // Hardware-specific library
 TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
 
+File pngfile;
+
+void * pngOpen(const char *filename, int32_t *size) {
+  Serial.printf("Attempting to open %s\n", filename);
+  pngfile = FileSys.open(filename, "r");
+  *size = pngfile.size();
+  return &pngfile;
+}
+
+void pngClose(void *handle) {
+  File pngfile = *((File*)handle);
+  if (pngfile) pngfile.close();
+}
+
+int32_t pngRead(PNGFILE *page, uint8_t *buffer, int32_t length) {
+  if (!pngfile) return 0;
+  page = page; // Avoid warning
+  return pngfile.read(buffer, length);
+}
+
+int32_t pngSeek(PNGFILE *page, int32_t position) {
+  if (!pngfile) return 0;
+  page = page; // Avoid warning
+  return pngfile.seek(position);
+}
 //====================================================================================
 //                                    Setup
 //====================================================================================
@@ -48,6 +78,7 @@ void setup()
   // Initialise the TFT
   tft.begin();
   tft.fillScreen(TFT_BLACK);
+  tft.initDMA();
 
   Serial.println("\r\nInitialisation done.");
 }
@@ -72,7 +103,7 @@ void loop()
         Serial.printf("image specs: (%d x %d), %d bpp, pixel type: %d\n", png.getWidth(), png.getHeight(), png.getBpp(), png.getPixelType());
         uint32_t dt = millis();
         if (png.getWidth() > MAX_IMAGE_WIDTH) {
-          Serial.println("Image too wide for allocated line buffer size!");
+          Serial.println("Image too wide for allocated lin buffer!");
         }
         else {
           rc = png.decode(NULL, 0);
@@ -98,6 +129,8 @@ void loop()
 // Callback function to draw pixels to the display
 void pngDraw(PNGDRAW *pDraw) {
   uint16_t lineBuffer[MAX_IMAGE_WIDTH];
+  static uint16_t dmaBuffer[MAX_IMAGE_WIDTH]; // static so buffer persists after fn exit
+
   png.getLineAsRGB565(pDraw, lineBuffer, PNG_RGB565_BIG_ENDIAN, 0xffffffff);
-  tft.pushImage(xpos, ypos + pDraw->y, pDraw->iWidth, 1, lineBuffer);
+  tft.pushImageDMA(xpos, ypos + pDraw->y, pDraw->iWidth, 1, lineBuffer, dmaBuffer);
 }
