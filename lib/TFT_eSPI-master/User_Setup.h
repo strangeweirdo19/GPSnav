a6815b09dece5c82f8e23d339494ac2a1f5f80f5
strangeweirdo19 <kirtivas.s2k1@gmail.com>
2025-04-09 21:46:07 +0530

    change 1

--- lib/TFT_eSPI-master/User_Setup.h
+++ lib/TFT_eSPI-master/User_Setup.h
@@ -209,21 +209,17 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-//#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
-//#define TFT_CS   15  // Chip select control pin
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 // For ESP32 Dev board (only tested with GC9A01 display)
 // The hardware SPI can be mapped to any pins
+//OG 
 #define TFT_DC   26  // Data Command control pin
 #define TFT_CS   25  // Reset pin (could connect to Arduino RESET pin)
 #define TFT_MOSI 23 // In some display driver board, it might be written as "SDA" and so on.
-#define TFT_RST  19  
-#define TFT_SCLK 18
+#define TFT_RST  18  
+#define TFT_SCLK 19
 #define TFT_BL   5  // LED back-light
 
 //#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
@@ -358,8 +354,8 @@
 // #define SPI_FREQUENCY   1000000
 // #define SPI_FREQUENCY   5000000
 // #define SPI_FREQUENCY  10000000
-// #define SPI_FREQUENCY  20000000
-#define SPI_FREQUENCY  27000000
+ #define SPI_FREQUENCY  20000000
+// #define SPI_FREQUENCY  27000000
 // #define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  55000000 // STM32 SPI1 only (SPI2 maximum is 27MHz)
 // #define SPI_FREQUENCY  80000000
